914e4bccb96d8548d942360eb58e6292b94330f1
Administrator <zblueline@163.com>
2021-04-07 15:49:26 +0800

    完成传统z做法

--- Water_Bottle/Assets/Scripts/WaterBottle.cs
+++ Water_Bottle/Assets/Scripts/WaterBottle.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,46 +8,19 @@ public class WaterBottle : MonoBehaviour
     Material Bottle;
     Vector3 LastPos;
     Vector3 forceDir;
-    // Vector3 LastFDir;
-    List<ForceDir> ForceList;
+    Vector3 LastFDir;
     
     float damping;
     int cnt;
-
-    public class ForceDir : IComparable<ForceDir>
-    {
-        public Vector3 forceDir;
-        public float selfdamping;
-
-        public ForceDir()
-        {
-            forceDir = new Vector3();
-            selfdamping = 1;
-        }
-        public ForceDir(Vector3 Force, float damping)
-        {
-            forceDir = Force;
-            selfdamping = damping;
-        }
-
-        public int CompareTo(ForceDir obj_)
-        {
-            if (this.forceDir.magnitude*this.selfdamping > obj_.forceDir.magnitude*obj_.selfdamping)
-                return 1;
-            else
-                return -1;
-        }
-    }
     // Start is called before the first frame update
     private void Awake() {
-        ForceList = new List<ForceDir>();
         cnt = 0;
         damping = 1;
     }
     void Start()
     {
         forceDir = new Vector3(0, 0, 0);
-        // LastFDir = new Vector3(0, 0, 0);
+        LastFDir = new Vector3(0, 0, 0);
         LastPos = transform.position;
         Bottle = gameObject.GetComponent<Renderer>().material;
     }
@@ -56,43 +28,22 @@ public class WaterBottle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(ForceList.Count > 0)
-            ForceList.Sort();
+        
         forceDir = transform.position - LastPos;
-
-        if(ForceList.Count < 10)
+        forceDir*=3;
+        if(forceDir.magnitude > LastFDir.magnitude * damping)
         {
-            ForceList.Add(new ForceDir(forceDir, 1));
+            LastFDir = forceDir;
+            damping = 1;
         }
-        else if(forceDir.magnitude > ForceList[0].forceDir.magnitude * ForceList[0].selfdamping)
-        {
-            ForceList.RemoveAt(0);
-            ForceList.Add(new ForceDir(forceDir, 1));
-        }
-        Vector3 LastFDir = new Vector3(0, 0, 0);
-        foreach(ForceDir t in ForceList)
-        {
-            // Debug.Log(t.forceDir.magnitude);
-
-            LastFDir += t.forceDir*t.selfdamping;
-
-            t.selfdamping *= 0.99f;
-        }
-        Debug.Log(LastFDir);
-
-
-        // if(forceDir.magnitude > LastFDir.magnitude * damping)
-        // {
-        //     LastFDir = forceDir;
-        //     damping = 1;
-        // }
-        // damping *= 0.99f;
+        damping *= 0.99f;
 
         LastPos = transform.position;
 
-        // Vector3 SinForceDir = damping * LastFDir * Mathf.Sin(SinSpeed*Time.time);
+        Vector3 SinForceDir = damping * LastFDir * Mathf.Sin(SinSpeed*Time.time);
 
-        Bottle.SetVector("_ForceDir", LastFDir);
+        Bottle.SetVector("_ForceDir", SinForceDir);
         Bottle.SetVector("_WorldZeroPos", transform.position);
+        Debug.Log(transform.position);
     }
 }
