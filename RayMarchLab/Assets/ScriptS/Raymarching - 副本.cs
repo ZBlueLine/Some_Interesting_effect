93ba57d335d8c437b6f89700111d90bdfe916c5b
Administrator <zblueline@163.com>
2021-06-07 18:31:16 +0800

    另一种做法

---
+++ RayMarchLab/Assets/ScriptS/Raymarching - 副本.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+
+[ExecuteInEditMode]
+public class Raymarching : MonoBehaviour
+{
+    [SerializeField]
+    private Shader RaymarchShader;
+
+    public Material RaymarchMaterial
+    {
+        get
+        {
+            if (!RaymarchMat && RaymarchShader)
+            {
+                RaymarchMat = new Material(RaymarchShader);
+                RaymarchMat.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return RaymarchMat;
+        }
+        set
+        {
+            RaymarchMat = value;
+        }
+    }
+    private Material RaymarchMat;
+
+    public Camera RayCamera
+    {
+        get
+        {
+            if (!Cam)
+                Cam = GetComponent<Camera>();
+            return Cam;
+        }
+        set
+        {
+            Cam = value;
+        }
+    }
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if(!RaymarchMat)
+        {
+            Graphics.Blit(source, destination);
+        }
+        RaymarchMat.SetMatrix("_CamFrustum", CamFrustum(Cam));
+        Graphics.Blit(source, destination , RaymarchMat);
+    }
+    private Camera Cam;
+
+    private Matrix4x4 CamFrustum(Camera cam)
+    {
+        Matrix4x4 frustum = Matrix4x4.identity;
+        float fov = Mathf.Tan((Cam.fieldOfView*0.5f)*Mathf.Deg2Rad);
+        Vector3 goUp = cam.transform.up * fov;
+        Vector3 goRight = cam.transform.right * fov * cam.aspect;
+
+        Vector3 TL = cam.transform.forward + goUp - goRight;
+        Vector3 TR = cam.transform.forward + goUp + goRight;
+        Vector3 BL = cam.transform.forward - goUp - goRight;
+        Vector3 BR = cam.transform.forward - goUp + goRight;
+
+        frustum.SetRow(0, TL.normalized);
+        frustum.SetRow(1, TR.normalized);
+        frustum.SetRow(2, BL.normalized);
+        frustum.SetRow(3, BR.normalized);
+
+        return frustum;
+    }
+}
---
+++ RayMarchLab/Assets/Shaders/RayMarchShader - 副本.shader
@@ -0,0 +1,73 @@
+﻿Shader "Custom/RayMarchShader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma target 3.0
+
+            #include "UnityCG.cginc"
+
+            float4x4 _CamFrustum;
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                float3 ray : TEXCOORD1;
+            };
+            
+            v2f vert (appdata v)
+            {
+                v2f o;
+                int index;
+                if(v.uv.x < 0.5)
+                {
+                    if(v.uv.y < 0.5)
+                            index = 2;
+                        else 
+                            index = 0;
+                }
+                else
+                {
+                    if(v.uv.y < 0.5)
+                        index = 3;
+                    else 
+                        index = 1;
+                }
+                float3 ray = _CamFrustum[index].xyz;
+                o.ray = ray;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _MainTex;
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed4 col = tex2D(_MainTex, i.uv);
+                // just invert the colors
+                col.rgb = normalize(i.ray);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
